1876d85b6b7cd2bc3f9e265dd95b46b92f279a4c
swethambarikv <93245352+swethambarikv@users.noreply.github.com>
2022-03-23 09:35:40 +0530

    Delete form.js

--- form.js
+++
@@ -1,8 +0,0 @@
-function validateform(){  
-    var name=document.formvalidation.name.value; 
-      
-    if (name==null || name==""){  
-      alert("Name can't be blank");  
-      return false;
-    } 
-}     
