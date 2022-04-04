c81ef719bb572a4ee8cb7de16eaae6ee1b5abb36
swethambarikv <93245352+swethambarikv@users.noreply.github.com>
2022-04-04 17:02:01 +0530

    Add files via upload

---
+++ ES6/generators.js
@@ -0,0 +1,24 @@
+const iterableObj = {
+    [Symbol.iterator]() {
+        let step = 0;
+        return {
+            next() {
+                step++;
+                if (step === 1) {
+                    return { value: '1', done: false};
+                 }
+                else if (step === 2) {
+                    return { value: '2', done: false};
+                }
+                else if (step === 3) {
+                    return { value: '3', done: false};
+                }
+                return { value: '', done: true };
+            }
+        }
+    }
+}
+
+for (const i of iterableObj) {
+ console.log(i);
+}
