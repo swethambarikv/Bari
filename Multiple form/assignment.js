a3cc1423382072259c42d1fb62b76253265f7448
swethambarikv <93245352+swethambarikv@users.noreply.github.com>
2022-04-19 09:01:27 +0530

    Update assignment.js

--- Multiple form/assignment.js
+++ Multiple form/assignment.js
@@ -1,270 +1,315 @@
-var step1 = document.getElementById("form1");
-var step2 = document.getElementById("form2");
-var step3 = document.getElementById("form3");
-var step4 = document.getElementById("form4");
-var step5 = document.getElementById("form5");
+var Form1 = document.getElementById("Form1");
+var Form2 = document.getElementById("Form2");
+var Form3 = document.getElementById("Form3");
+var Form4 = document.getElementById("Form4");
+var Form5 = document.getElementById("Form5");
+var Form6 = document.getElementById("Form6");
 
-var next1 = document.getElementById("next1");
-var next2 = document.getElementById("next2");
-var next3 = document.getElementById("next3");
-var next4 = document.getElementById("next4");
-
-var back1 = document.getElementById("back1");
-var back2 = document.getElementById("back2");
-var back3 = document.getElementById("back3");
-var back4 = document.getElementById("back4");
-
-var progress = document.getElementById("progress");
-
-
-function pageChange()
-{
-    if(pageOneCheck1==1 && pageOneCheck2==1 && pageOneCheck3==1 && pageOneCheck4==1 && pageOneCheck5==1 ){
-        next1.onclick=function(){
-    
-            form1.style.left="-450px";
-            form2.style.left="-40px";
-            progress.style.width="40px";
-        }
-    }
-    // else{
-    //     alert("Enter the details correctly!!");
-    // }
+Back1.onclick = function(){
+    Form1.style.left = "40px";
+    Form2.style.left = "450px";
+    progress.style.width = "80px";
 }
 
-var email = document.getElementById("email");
-var pass = document.getElementById("pass1");
-var cPass = document.getElementById("pass2");
-var fname = document.getElementById("fname");
-var lname = document.getElementById("lname");
+Back2.onclick = function(){
+    Form2.style.left = "40px";
+    Form3.style.left = "450px";
+    progress.style.width = "160px";
+}
+Next3.onclick = function(){
+    Form3.style.left = "-450px";
+    Form4.style.left = "40px";
+    progress.style.width = "200px";
+}
+Back3.onclick = function(){
+    Form3.style.left = "40px";
+    Form4.style.left = "450px";
+    progress.style.width = "240px";
+}
 
-var alphaNumericFormat = "/([a-zA-Z0-9-_\.]+)@([a-zA-Z0-9]+)\.([a-zA-Z]{2,5})(\.[a-zA-Z]{2,5})?$/";
-var alphaFormat = "/^[a-zA-Z ]+$/";
+Back4.onclick = function(){
+    Form4.style.left = "40px";
+    Form5.style.left = "450px";
+    progress.style.width = "320px";
+}
 
-var pageOneCheck1 = 0;
-var pageOneCheck2 = 0;
-var pageOneCheck3 = 0;
-var pageOneCheck4 = 0;
-var pageOneCheck5 = 0;
+var field1=0;
+var field2=0;
+var field3=0;
+var field4=0;
+var field5=0;
 
-/*back1.onclick = function () {
-    form1.style.left = "40px";
-    form2.style.left = "450px";
-    progress.style.width = "72px";
-
-}*/
-
-// back2.onclick = function () {
-//     form2.style.left = "40px";
-//     form3.style.left = "450px";
-//     progress.style.width = "144px";
-// }
-
-// back3.onclick = function () {
-//     form3.style.left = "40px";
-//     form4.style.left = "450px";
-//     progress.style.width = "216px";
-// }
-// next3.onclick = function () {
-//     form3.style.left = "-450px";
-//     form4.style.left = "40px";
-//     progress.style.width = "288px";
-
-// }
-
-// back4.onclick = function () {
-//     form4.style.left = "40px";
-//     form5.style.left = "450px";
-//     progress.style.width = "288px";
-// }
-
-/* EMAIL*/    
-function validateEmail()
-{
-    // var emails = document.getElementById('email');
-    var filter ="/^([a-zA-Z0-9_\.\-]){5,25}+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/";
-if(email==""){
-    alert("Email required");
-    pageOneCheck1=0;
+function validateMail(){
+    var emailregex=/^[a-zA-Z0-9+_.-]+@[a-zA-Z.-]+.[a-z]{2,3}$/;
+    var email=document.getElementById("mail").value;
+    if(email=="")
+    {
+    alert("Enter email");
+    field1=0;
     return false;
-}
-    else if (!filter.match(email.value)) {
-        alert('Please provide a valid email address');
-        // emails.focus;
-        pageOneCheck1=0;
-        return false;
+    }
+    else if(!emailregex.test(email))
+    {
+        alert("Enter a valid email");
+    field1=0;
+    return false;
     }
     else{
-        pageOneCheck1=1;
+        field1=1;
     }
 }
-/* NAME*/
-function validateName() {
-    var regName = "/^[A-Za-z]+$/";
-    // var name = document.getElementById('fname').value;
-    // if (regName.test(fname.value)==true) {
-    //     alert('Please enter your full name (first & last name).');
-    //     // document.getElementById('fname').focus();
-    //     return false;
-    // } else {
-    //     alert('Valid name given.');
-    //     return true;
-    // }
-    if(!regName.match(fname.value)){
-        alert("Only Alphabets are allowed");
-        return false;
-        pageOneCheck2=0;
+function validatePass(){
+    var pswd=document.getElementById("password").value;
+    if(pswd=="")
+    {
+    alert("Enter password");
+    return false;
+    field4=0;
     }
     else{
-        pageOneCheck2=1;
+        field4=1;
     }
 }
-/* PASSWORD*/
-function verifyPassword() {
+
+function validateConfirm(){
+    var cfrmpass=document.getElementById("cfrmpass").value;
+    var pswd=document.getElementById("password").value;
+    if(pswd != cfrmpass)
+    {
+        alert("Passwords didn't match");
+        return false;
+        field5=0;
+    }
+    else{
+        field5=1;
+    }
+}
+
+function validateName()
+{
+    var fname=document.getElementById("fname").value;
+    var nameregex=/^[a-zA-Z]{3,15}$/;
+
+    if(!nameregex.test(fname))
+    {
+    alert("name should be in character");
+    return false;
+    field2=0;
+    } 
+    else{
+        field2=1;
+    }
+}
+
+function validateLname(){
+    var lname=document.getElementById("lname").value;
+    var nameregex=/^[a-zA-Z]{3,15}$/;
+        if(lname == "")
+        {
+            alert("enter the last name");
+        return false; 
+        field3=0; 
+        }
+
+        if(!nameregex.test(lname))
+        {
+        alert("name should be in character");
+        return false;
+        field3=0;
+        }
+        else{
+            field3=1;
+        }
+}
+
+
+function pageChange1()
+{
+    if(field1==1 && field2==1 && field3==1 && field4==1 && field5==1 )
+    {
+        Next1.onclick = function(){
+                Form1.style.left = "-450px";
+                Form2.style.left = "40px";
+                progress.style.width = "40px";
+            }
+    }
+    else{
+        alert("Please fill the required details!");
+    }
+}
+
+
+var field21=0;
+var field22=0;
+var field23=0;
+
+function validateDob()
+{
+
+    if(validateAge()<18)
+    {
+        alert("Age should be greater than 18");
+        return false;
+        field21=0;
+    }
+    else{
+        field21=1;
+    }
+}
+function validateAge()
+{
+var d_o_b=document.getElementById("dob").value;
+var dob = new Date(d_o_b);  
+var month_diff = Date.now() - dob.getTime();   
+var age_dt = new Date(month_diff);       
+var year = age_dt.getUTCFullYear(); 
+var age = Math.abs(year - 1970);
+return age;
+}
+function validateState()
+{
+    var stateVal=document.getElementById("state").value;
+    if(stateVal=="Select a state")
+        {
+        alert("select a state");
+        return false;
+        field22=0;
+        }
+        else{
+            field22=1;
+        }
+}
+function validatePhone()
+{
+    var phoneNo=document.getElementById("phonenum").value;
+    var phoneregex=/^[6-9]{1}[0-9]{9}$/
+        if(phoneNo == "")
+        {
+        alert("enter phone no.");
+        return false;
+        field23=0;
+        }
+      
+        else if(!phoneregex.test(phoneNo))
+        {
+            alert("Number should start with 6/7/8/9 and contain 10 digits");
+            field23=0;
+        }
+        else{
+            field23=1;
+        }
+}
+function pageChange2(){
+    if(field21==1 && field22==1 && field23==1)
+    {
+        Next2.onclick = function(){
+                Form2.style.left = "-450px";
+                Form3.style.left = "40px";
+                progress.style.width = "120px";
+            }
+    }
+else{
+    alert("Please fill all the required details!");
+}
+}
+
+
+
+var field41 = 0;
+var field42 = 0;
+
+
+function validateGrade()
+{
+    var grade=document.getElementById("grades").value;
+    if(grade=="Select a grade")
+        {
+        alert("select a grade");
+        return false;
+        field41=0;
+        }
+        else{
+            field41=1;
+        }
+}
+
+function validateYOP() {
+    var yopFormat = /^(0[1-9]|1[012])[\/\-](19[5-9]\d|20[0-2]\d)$/;
+    var yop = document.getElementById("pass").value;
+    if(yop==""){
+        alert("This field is required");
+        return false;
+        field42=0;
+    }
+    else if(!yopFormat.test(yop))
+    {
+        alert("Enter a valid pattern");
+        return false;
+        field42=0;
+    }
+    else{
+        field42=1;
+    }
+}
+
+function pageChange4() {
+    if(field41 == 1 && field42 == 1){
+        Next4.onclick = function(){
+                Form4.style.left = "-450px";
+                Form5.style.left = "40px";
+                progress.style.width = "280px";
+            }
+    } 
+    else{
+        alert("Please fill all the required details!");
+    }
+}
+
+var checkBox = document.getElementById('check');
+var field51=0;
+var field52=0;
+function validateDept()
+{
+    var dept = document.getElementById('dept').value;
+    console.log(dept);
+    if(dept=="Select a Department")
+    {
+    alert("select a Department");
+    return false;
+    field51=0;
+    }
+    else{
+        field51=1;
+    } 
+}
+
+function validateCheck()
+{
+    var check=document.getElementById("tnc").checked;
+    if(check==false)
+    {
+        alert("Please accept terms and conditions");
+        return false;
+        field52=0;
+    }
+    else{
+        field52=1;
+    }
+
+}
+function pageChange5(){
    
-    var pass=document.getElementById("pass1").value;
-    if(pass==" "){
-        alert("Password Required");
-        return false;
-        pageOneCheck4=0;
-    }
-    else{
-        pageOneCheck4=1;
-    }
+    console.log(field51);
+    if(field51 == 1 && field52 == 1){
+        Submit1.onclick = function(){
+    Form5.style.left = "-450px";
+    Form6.style.left = "40px";
+    progress.style.width = "360px";
 }
-/*CONFIRM PASSWORD*/
-function matchPassword(){
-    var cpass=document.getElementById("pass2").value;
-    var pass=document.getElementById("pass1").value;
-    if(pass!=cpass){
-        alert("Password doesnt match.Re-enter");
-        return false;
-        pageOneCheck5=0;
-    }
-    else{
-        pageOneCheck5=1;
-    }
+    } 
+    
+
 }
-
-
-// function verifyPassword() {
-// if (!password.value.length) {
-//     document.getElementById("pass1").style.border = "2px solid red";
-//     document.getElementById("pass").innerHTML = "This field is neccessary";
-//     pageOneCheck4 = 0;
-
-//     return false;
-// }
-// else {
-//     document.getElementById("pass").innerHTML = " ";
-//     document.getElementById("pass1").style.border = "2px solid green";
-//     pageOneCheck4 = 1;
-
-// }
-// }
-
-    // if (confirmPassword.value.length==true) {
-    //     document.getElementById("pass2").style.border = "2px solid red";
-    //     document.getElementById("cpass").innerHTML = "Please Re-Enter the password";
-    //     pageOneCheck5 = 0;
-
-    //     return false;
-    // }
-    // else if (password.value != confirmPassword.value) {
-    //     document.getElementById("cpass").innerHTML = "Passwords did not match";
-    //     document.getElementById("pass2").style.border = "2px solid red";
-    //     pageOneCheck5 = 0;
-
-    //     return false;
-
-    // }
-    // else {
-    //     document.getElementById("cpass").innerHTML = " ";
-    //     document.getElementById("pass2").style.border = "2px solid green";
-    //     pageOneCheck5 = 1;
-
-    // }
-
-// function changePage1() {
-//     if (pageOneCheck1 == 1 && pageOneCheck2 == 1 && pageOneCheck3 == 1 && pageOneCheck4 == 1 && pageOneCheck5 == 1) {
-//         next1.onclick = function () {
-//             form1.style.left = "-450px";
-//             form2.style.left = "40px";
-//             progress.style.width = "144px";
-//         }
-//     }
-//     else {
-//         alert("Please fill all the required details to move to the next step");
-//     }
-
-// }
-function validateDOB() {
-    var lblError = document.getElementById("lblError");
-
-    //Get the date from the TextBox.
-    var dateString = document.getElementById("dob").value;
-    var regex = "/(((0|1)[0-9]|2[0-9]|3[0-1])\/(0[1-9]|1[0-2])\/((19|20)\d\d))$/";
-
-    //Check whether valid dd/MM/yyyy Date Format.
-    if (regex.test(dateString)) {
-        var parts = dateString.split("/");
-        var dtDOB = new Date(parts[1] + "/" + parts[0] + "/" + parts[2]);
-        var dtCurrent = new Date();
-        lblError.innerHTML = "Eligibility 18 years ONLY."
-        if (dtCurrent.getFullYear() - dtDOB.getFullYear() < 18) {
-            return false;
-        }
-
-        if (dtCurrent.getFullYear() - dtDOB.getFullYear() == 18) {
-
-            //CD: 11/06/2018 and DB: 15/07/2000. Will turned 18 on 15/07/2018.
-            if (dtCurrent.getMonth() < dtDOB.getMonth()) {
-                return false;
-            }
-            if (dtCurrent.getMonth() == dtDOB.getMonth()) {
-                //CD: 11/06/2018 and DB: 15/06/2000. Will turned 18 on 15/06/2018.
-                if (dtCurrent.getDate() < dtDOB.getDate()) {
-                    return false;
-                }
-            }
-        }
-        lblError.innerHTML = "";
-        return true;
-    } else {
-        lblError.innerHTML = "Enter date in dd/MM/yyyy format ONLY."
-        return false;
-    }
-}
-function checkPhone() {
-    var phone = document.forms["form2"]["phone"].value;
-    var phoneNum = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-    if (phone.value.match(phoneNum)) {
-        return true;
-    }
-    else {
-        document.getElementById("phone").className = document.getElementById("phone").className + " error";
-        return false;
-    }
-}
-function thank(){
-    alert("Thank you for Registering");
-}
-
-function changePage() {
-    const form = document.querySelectorAll('form');
-
-    console.log("in chg")
-
-    form.forEach(f => {
-        f.classList.add('active');
-    })
-}
-
-
-
-
-
-
 
 
 
